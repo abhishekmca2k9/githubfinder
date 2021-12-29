84ae08712d705654874bc323924b8ec91558cd2c
Abhishek Tiwari <56433961+abhishekmca2k9@users.noreply.github.com>
2021-12-30 00:37:22 +0530

    initial comit

---
+++ src/components/layout/DSpinner.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+import spinner from './spinner.gif';
+
+export const DSpinner = () => {
+    return (
+        <Fragment>
+            <img src={spinner} alt="loading..." style={{ width:"200px", margin: "auto", display: "block"}} />
+        </Fragment>
+    )
+}
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import Search from '../users/Search';
+
+import Users from '../users/Users';
+
+export const Home = () => {
+    return (
+        <Fragment>
+            <Search />
+            <Users />
+        </Fragment>
+    )
+}
